1558c4d31eb62e180ad53c128b5015cb2b50638d
D4rksea <65219077+D4rksea@users.noreply.github.com>
2023-09-23 10:16:14 +0200

    prova.py
    cancello

--- prova.py
+++ prova.py
@@ -1,17 +1,1 @@
-conto=110
-prel=10
-while conto<prel:
-    prel1=input("inserisci quanti soldi vuoi prelevare ")
-    if prel1.isnumeric() == True:
-        prel=float(prel1)
-        print(prel1+" prel")
-        break
-    else:
-        print("Inserisci un numero, te lo richiedo:")
-        prel=100000000000000000
-if conto>prel:
-    print("conto insufficiente per eseguire l'operazione")
-else:
-    conto-prel
-print(conto)
-print(prel1+" prel")
+
